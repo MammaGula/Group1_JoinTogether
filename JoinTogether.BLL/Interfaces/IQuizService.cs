66de89e66896ab59b1d371a4abaa8c98c8a3547f
MammaGula <supaphit.rue2@gmail.com>
2026-08-31 10:51:59 +0200

    Created quiz logics enligt locations in BLL

---
+++ JoinTogether.BLL/Interfaces/IQuizService.cs
@@ -0,0 +1,12 @@
+﻿using JoinTogether.Shared.DTOs;
+
+namespace JoinTogether.BLL.Interfaces;
+
+public interface IQuizService
+{
+    // Fetch questions for a given location (options only, no correct-answer flag)
+    Task<List<QuizQuestionDto>?> GetQuestionsByLocationAsync(int locationId);
+
+    // Validate submitted answers and calculate + persist the score
+    Task<QuizResultDto> SubmitQuizAsync(string userId, SubmitQuizDto submission);
+}
