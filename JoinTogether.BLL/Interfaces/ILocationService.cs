3c218072587980417724b091e193ee72dc78c82e
MammaGula <supaphit.rue2@gmail.com>
2026-09-01 00:23:05 +0200

    After following the new backlog, I created the new BLL based only on US-1, so we will go step by step after each user story. >>> So this version is BLL only for US-1

---
+++ JoinTogether.BLL/Interfaces/ILocationService.cs
@@ -0,0 +1,12 @@
+﻿using JoinTogether.Shared.DTOs;
+
+namespace JoinTogether.BLL.Interfaces;
+
+public interface ILocationService
+{
+    // US-01: browse all locations (with quiz info)
+    Task<List<LocationSummaryDto>> GetAllLocationsAsync();
+
+    // US-01: view a single location's details
+    Task<LocationDetailDto?> GetLocationByIdAsync(int locationId);
+}
---
+++ JoinTogether.BLL/Services/LocationService.cs
@@ -0,0 +1,51 @@
+﻿using JoinTogether.BLL.Interfaces;
+using JoinTogether.DAL.Repositories;
+using JoinTogether.Shared.DTOs;
+
+namespace JoinTogether.BLL.Services;
+
+public class LocationService : ILocationService
+{
+    private readonly ILocationRepository _locationRepository;
+
+    public LocationService(ILocationRepository locationRepository)
+    {
+        _locationRepository = locationRepository;
+    }
+
+    // --- Browse all locations ---
+    public async Task<List<LocationSummaryDto>> GetAllLocationsAsync()
+    {
+        var locations = await _locationRepository.GetAllAsync();
+
+        return locations.Select(l => new LocationSummaryDto
+        {
+            Id = l.Id,
+            Name = l.Name,
+            Description = l.Description,
+            Latitude = l.Latitude,
+            Longitude = l.Longitude,
+            Category = l.Category,
+            QuizQuestionCount = l.QuizQuestions?.Count ?? 0
+        }).ToList();
+    }
+
+    // --- View a single location ---
+    public async Task<LocationDetailDto?> GetLocationByIdAsync(int locationId)
+    {
+        var location = await _locationRepository.GetWithQuizAsync(locationId);
+        if (location == null)
+            return null;
+
+        return new LocationDetailDto
+        {
+            Id = location.Id,
+            Name = location.Name,
+            Description = location.Description,
+            Latitude = location.Latitude,
+            Longitude = location.Longitude,
+            Category = location.Category,
+            QuizQuestionCount = location.QuizQuestions.Count
+        };
+    }
+}
