92711db0270f836a759e505997cd9132d63f72e4
MammaGula <supaphit.rue2@gmail.com>
2026-09-01 10:36:16 +0200

    AuthController

--- JoinTogether.API/Controllers/AuthController.cs
+++ JoinTogether.API/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JoinTogether.API.Controllers;
 
+// AuthController handles user authentication, including registration and login.
 [ApiController]
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
