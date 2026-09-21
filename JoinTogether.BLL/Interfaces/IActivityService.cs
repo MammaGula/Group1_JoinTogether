4e1543007f35630e704b96abad9e628a60032fe0
MammaGula <supaphit.rue2@gmail.com>
2026-09-21 12:26:11 +0200

    Implemented JoinFuctionality and updated participantCount

---
+++ JoinTogether.BLL/Interfaces/IActivityService.cs
@@ -0,0 +1,15 @@
+﻿using JoinTogether.Shared.DTOs;
+
+namespace JoinTogether.BLL.Interfaces;
+
+public interface IActivityService
+{
+    // Create an activity at a location (requires having passed that location's quiz)
+    Task<ActivityDto> CreateActivityAsync(string userId, CreateActivityDto request);
+
+    // View all activities tied to a location
+    Task<List<ActivityDto>> GetActivitiesByLocationIdAsync(int locationId);
+
+    // Join an existing activity
+    Task JoinActivityAsync(string userId, int activityId);
+}
